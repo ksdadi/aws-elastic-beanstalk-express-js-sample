9ff70165e7e861c2f7b0c092e06b24e8f9b92ea3
kumar <kumaraswamydadi@gmail.com>
2021-02-20 13:27:55 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hi kumar'));
+app.get('/', (req, res) => res.send('hi swamy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
